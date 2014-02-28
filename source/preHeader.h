0fccf7d4b36a32b150cbfb486a50997ad68bf1fe
Donny Reynolds <donnywith@gmail.com>
2014-02-28 14:19:03 -0800

    added preheader.h

---
+++ source/preHeader.h
@@ -0,0 +1,13 @@
+#ifndef Assignment2_preHeader_h
+#define Assignment2_preHeader_h
+
+#include <iostream>
+#include <Eigen> //Eigen needs to go before CImg because X11 re-defines "Success" macro
+#include <CImg.h>
+#include <FreeImage.h>
+
+using namespace std;
+using namespace Eigen;
+using namespace cimg_library;
+
+#endif
