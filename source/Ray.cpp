bb5032a07ca82cc960ff31e3f0abd1e3d96449f8
Donny Reynolds <donnywith@gmail.com>
2014-03-02 14:09:11 -0800

    implemented basics of Ray class

---
+++ source/Ray.cpp
@@ -0,0 +1,23 @@
+#include "preHeader.h"
+
+class Ray {
+public:
+    Point position;
+    Vector direction;
+    float t_min, t_max;
+    
+    Ray() {
+        position = Point(0,0,0);
+        direction = Vector(0,0,0);
+        t_min = 0.0;
+        t_max = 1.0;
+    }
+    
+    Ray(Point start, Vector direction, min, max) {
+        position = start;
+        this.direction = direction;
+        t_min = min;
+        t_max = max;
+    }
+};
+
