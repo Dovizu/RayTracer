e18c601c189238142b4a9844de2ece6f8989f2eb
Donny Reynolds <donnywith@gmail.com>
2014-03-10 00:52:40 -0700

    scene.cpp is being tracked again

--- source/Scene.cpp
+++ source/Scene.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
         }
         if (result.optName.compare("-tparser") == 0) {
             string basePath = result.args->at(0);
-            testParser(basePath); //yolo
+            testParser(basePath);
         }
     }
     return 0;
