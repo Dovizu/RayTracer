aaee395fe02758c23889a9c4a1997c7c19fe57f3
Donny Reynolds <donnywith@gmail.com>
2014-03-10 00:49:23 -0700

    temporarily deleted scene.cpp to fix something

--- source/scene.cpp
+++ source/scene.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
         }
         if (result.optName.compare("-tparser") == 0) {
             string basePath = result.args->at(0);
-            testParser(basePath); //yolo
+            testParser(basePath); //yolo  djklfsfjdkslj
         }
     }
     return 0;
