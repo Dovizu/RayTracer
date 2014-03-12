68f6843c7ab483edd3e53c22508126c5f93a5dd3
lcolobong <lcolobong@berkeley.edu>
2014-03-11 23:08:04 -0700

    small changes to light

--- source/Light.cpp
+++ source/Light.cpp
@@ -31,7 +31,7 @@ void Light::generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor)
     if (lightType == LightSourcePoint) {
         Point localpos = local.position;
         Vector lvec = makeVec(localpos, location);
-        lray->position = location;
+        lray->position = localpos;
         lray->direction = lvec;
         lray->t_min = 0.01f;
         lray->t_max = 1.0;
