054a207cc7342b4f38903d3e8b04956fe00feb34
lcolobong <lcolobong@berkeley.edu>
2014-03-14 17:18:22 -0700

    basic anti-aliasing -- untested

--- source/Sampler.cpp
+++ source/Sampler.cpp
@@ -3,11 +3,13 @@
 
 class Sampler {
     int xPixels, yPixels;
-    int xIndex, yIndex;
+    float xIndex, yIndex, unit, offset;
+    int sqrt_rpp; //The square root of rays per pixel
     
 public:
     Sampler();
     Sampler(int xPix, int yPix);
+    Sampler(int xPix, int yPix,float sqrt_rpp);
     bool getSample(Sample* s);
 };
 
@@ -15,21 +17,37 @@ Sampler::Sampler()
 {
     xPixels = 320;
     yPixels = 320;
-    xIndex = 0;
-    yIndex = 0;
+    xIndex = 0.0f;
+    yIndex = 0.0f;
+    sqrt_rpp = 1.0f;
+    unit = 1.0f;
+    offset = 0.5f;
 }
 
 Sampler::Sampler(int xPix, int yPix)
 {
     xPixels = xPix;
     yPixels = yPix;
-    xIndex = 0;
+    xIndex = 0.0f;
+    yIndex = 0.0f;
+    unit = 1.0f;
+    offset = 0.5f;
+    sqrt_rpp = 1.0f;
+}
+
+Sampler::Sampler(int xPix, int yPix, float sqrt_rpp)
+{
+    xPixels = xPix;
+    yPixels = yPix;
     yIndex = 0;
+    xIndex = 0;
+    this->sqrt_rpp = sqrt_rpp;
+    unit = 1.0f/sqrt_rpp;
+    offset = unit/2;
 }
 
 bool Sampler::getSample(Sample* s)
 {
-    float offset = 0.5;
     if (xIndex >= xPixels && yIndex >= yPixels) {
         return false;
     }
@@ -37,16 +55,16 @@ bool Sampler::getSample(Sample* s)
     {
         (*s)(0) = xIndex + offset;
         (*s)(1) = yIndex + offset;
-        xIndex++;
+        xIndex+=unit;
         return true;
     }
     if (xIndex >= xPixels) {
         xIndex = 0;
-        yIndex++;
+        yIndex+=unit;
         if (yIndex < yPixels) {
             (*s)(0) = xIndex + offset;
             (*s)(1) = yIndex + offset;
-            xIndex++;
+            xIndex+=unit;
             return true;
         }
         return false;
